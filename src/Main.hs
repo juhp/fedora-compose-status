f5c62a1fb1e4018e94497bf0f3c20d1dc46e20ac
Jens Petersen <juhpetersen@gmail.com>
2022-05-18 17:34:11 +0800

    add --recent and put one compose per line

--- src/Main.hs
+++ src/Main.hs
@@ -5,7 +5,7 @@ module Main (main) where
 import Control.Monad.Extra (forM_, when, whenJustM, (>=>))
 import qualified Data.ByteString.Lazy.Char8 as B
 import Data.Char
-import Data.List.Extra (lower)
+import Data.List.Extra (lower, takeEnd)
 import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
@@ -14,7 +14,8 @@ import Network.HTTP.Directory
 import Network.HTTP.Simple
 import SimpleCmdArgs
 
-data Snapshot = Latest | Newest | Snap String
+data Snapshot = Latest | Newest | Snap String | Compose Text | Recent
+  deriving Eq
 
 -- FIXME branched
 main :: IO ()
@@ -37,7 +38,8 @@ main =
   where
     snapOpt =
       Snap <$> strArg "SNAPSHOT" <|>
-      flagWith Newest Latest 'l' "latest" "latest finished snapshot"
+      flagWith' Latest 'l' "latest" "latest finished snapshot" <|>
+      flagWith Newest Recent 'r' "recent" "last 10 snapshots"
 
 topUrl :: String
 topUrl = "https://kojipkgs.fedoraproject.org/compose"
@@ -49,33 +51,40 @@ listCmd mrelease =
 getComposes :: Maybe String -> IO [Text]
 getComposes mrelease = do
   let release = maybe "rawhide" lower mrelease
-  httpDirectory' $ topUrl +/+ release
+  filter (not . (T.pack "latest-" `T.isPrefixOf`)) <$>
+    httpDirectory' (topUrl +/+ release)
 
 newestCompose :: Maybe String -> IO String
 newestCompose mrelease = do
   composes <- getComposes mrelease
-  return $ T.unpack $ last $ filter (not . (T.pack "latest-" `T.isPrefixOf`)) composes
+  return $ T.unpack $ last composes
 
 statusCmd :: Bool -> Maybe String -> Snapshot -> IO ()
 statusCmd debug mrelease snapshot = do
   let release = maybe "rawhide" lower mrelease
-  fetchFile release
+  if snapshot == Recent
+    then
+    getComposes mrelease >>=
+    mapM_ (statusCmd debug mrelease . Compose) . takeEnd 10
+    else checkStatus release
   where
-    fetchFile release = do
+    checkStatus release = do
       snap <-
         case snapshot of
           Latest -> return $ "latest-Fedora-" ++ capitalize release
-          Newest -> newestCompose mrelease
           Snap snap -> return $ "Fedora-" ++ capitalize release ++ '-' : snap
+          Compose comp -> return $ T.unpack comp
+          _ -> newestCompose mrelease
       let snapurl = topUrl +/+ release +/+ snap
       when debug $ putStrLn snapurl
       forM_ ["COMPOSE_ID", "STATUS"] $ \file -> do
         resp <- parseRequest (snapurl +/+ file) >>= httpLBS
-        B.putStr $ getResponseBody resp
-        when (file == "COMPOSE_ID") $ putChar '\n'
+        B.putStr $ removeFinalNewLine $ getResponseBody resp
+        putChar ' '
       whenJustM (httpLastModified' (snapurl +/+ "STATUS")) $
         utcToLocalZonedTime >=> print
 
+    removeFinalNewLine bs = if B.last bs == '\n' then B.init bs else bs
 capitalize :: String -> String
 capitalize "" = ""
 capitalize (h:t) = toUpper h : t
