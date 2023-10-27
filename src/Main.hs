93fe420711142efa0ffb7252abff2dc5bca5ff00
Jens Petersen <juhpetersen@gmail.com>
2023-10-27 18:13:04 +0800

    promptKeyPress for more compose statuses
    nicer UX

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,14 @@
 -- SPDX-License-Identifier: BSD-3-Clause
+{-# Language LambdaCase #-}
 
 module Main (main) where
 
-import Control.Monad.Extra (when)
+import Control.Monad.Extra (foldM_, when)
 import qualified Data.ByteString.Lazy.Char8 as B
 import Data.Char ( isDigit )
 import Data.Functor ((<&>))
-import Data.List.Extra (lower, nub, sort, {-sortOn,-} takeEnd)
-import Data.Maybe (isJust)
+import Data.List.Extra (isPrefixOf, lower, nub, sort, takeEnd)
+import Data.Maybe (isJust, isNothing)
 import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
@@ -17,8 +18,12 @@ import Network.HTTP.Directory
 import Network.HTTP.Simple
     ( parseRequest, getResponseBody, httpLBS )
 import SimpleCmdArgs
+import SimplePrompt
+import System.Console.ANSI
 
 import Paths_fedora_composes (version)
+
+data Limit = NoLimit | Limit Int
 
 main :: IO ()
 main =
@@ -30,7 +35,7 @@ main =
     "List dirs/composes (by default only last compose)" $
     listCmd
     <$> debugOpt
-    <*> limitOpt
+    <*> optional limitOpt
     <*> switchWith 'r' "repos" "Only list target repos"
     <*> optional dirOpt
     <*> optional snapOpt
@@ -38,7 +43,7 @@ main =
     "Show compose status" $
     statusCmd
     <$> debugOpt
-    <*> limitOpt
+    <*> optional limitOpt
     <*> dirOpt
     <*> optional snapOpt
   ]
@@ -46,8 +51,8 @@ main =
     debugOpt = switchWith 'd' "debug" "debug output"
 
     limitOpt =
-      flagWith' Nothing 'a' "all-composes" "All composes" <|>
-      Just <$> optionalWith auto 'l' "limit" "LIMIT" "Number of composes (default: 10)" 10
+      flagWith' NoLimit 'a' "all-composes" "All composes" <|>
+      Limit <$> optionWith auto 'l' "limit" "LIMIT" "Max number of composes"
 
     dirOpt = strArg "DIR"
 
@@ -59,7 +64,7 @@ topUrl = "https://kojipkgs.fedoraproject.org/compose"
 httpDirectories :: String -> IO [Text]
 httpDirectories = fmap (map noTrailingSlash) . httpDirectory'
 
-listCmd :: Bool -> Maybe Int -> Bool -> Maybe String
+listCmd :: Bool -> Maybe Limit -> Bool -> Maybe String
         -> Maybe String -> IO ()
 listCmd _ _ _ Nothing _ =
   httpDirectories topUrl >>= mapM_ T.putStrLn
@@ -80,7 +85,7 @@ showCompose (Compose d r) = T.unpack r <> "-" <> T.unpack d
 
 -- data RepoComposes = RepoComposes Text [Text]
 
-getComposes :: Bool -> Maybe Int -> Bool -> FilePath
+getComposes :: Bool -> Maybe Limit -> Bool -> FilePath
             -> Maybe String -> IO [String]
 getComposes debug mlimit onlyrepos dir mpat = do
   let url = topUrl +/+ dir
@@ -93,48 +98,65 @@ getComposes debug mlimit onlyrepos dir mpat = do
     filter (\c -> isDigit (T.last c) && T.any (== '.') c) <$>
     httpDirectories url
   when debug $ print $ repocomposes
-  return $ selectRepos url repocomposes
+  return $ reverse $ selectRepos url repocomposes
   where
     selectRepos :: String -> [Compose] -> [String]
     selectRepos url =
       if onlyrepos
-      then map T.unpack . nub . map compRepo -- FIXME this is wrong
+      then map T.unpack . nub . map compRepo
       else map ((url +/+) . showCompose)
 
     repoSubset :: [Compose] -> [Compose]
     repoSubset = maybe id (\n -> filter ((T.pack (lower n) `T.isInfixOf`) . T.toLower . compRepo)) mpat
 
-    limitComposes = maybe id takeEnd mlimit
+    limitComposes =
+      flip (maybe (takeEnd 10)) mlimit $
+      \case
+        Limit n -> takeEnd n
+        NoLimit -> id
 
---    sortRelease = sortOn (T.takeWhileEnd (/= '-') . compRepo)
-
--- FIXME sort output by timestamp
-statusCmd :: Bool -> Maybe Int -> FilePath -> Maybe String
+statusCmd :: Bool -> Maybe Limit -> FilePath -> Maybe String
           -> IO ()
 statusCmd debug mlimit dir mpat = do
   tz <- getCurrentTimeZone
   getComposes debug mlimit False dir mpat >>=
-    mapM (checkStatus tz) >>= mapM_ putStrLn
+    foldM_ (checkStatus tz) (Just False)
   where
-    checkStatus tz snapurl = do
---      putChar ' '
-      -- FIXME use formatTime
-      mstart <- httpMaybeLastModified $  snapurl +/+ "COMPOSE_ID"
-      mfinish <- httpMaybeLastModified $  snapurl +/+ "STATUS"
-      status <-
-        if isJust mfinish
-        then B.unpack <$> getComposeFile snapurl "STATUS"
-        else return "STATUS missing"
-      return $ unlines $
-        snapurl :
-        [maybe "" show mstart | status /= "STARTED"] ++
-        [maybe "" show mfinish ++ " " ++ status]
-        where
-          httpMaybeLastModified url = do
-            exists <- httpExists' url
-            if exists
-              then fmap (utcToZonedTime tz) <$> httpLastModified' url
-              else return Nothing
+    checkStatus tz mfinished snapurl = do
+      if mfinished == Just True
+        then return (Just True)
+        else do
+        more <-
+          if isNothing mfinished
+          then do
+            -- FIXME better haskeline
+            ok <- promptKeyPress "Press key for more or Ctrl-d to finish:"
+            cursorUp 1
+            return ok
+          else putChar '\r' >> return True
+        if not more
+          then return (Just True)
+          else do
+          -- FIXME use formatTime?
+          mstart <- httpMaybeLastModified $  snapurl +/+ "COMPOSE_ID"
+          mfinish <- httpMaybeLastModified $  snapurl +/+ "STATUS"
+          status <-
+            if isJust mfinish
+            then B.unpack <$> getComposeFile snapurl "STATUS"
+            else return "STATUS missing"
+          putStrLn $ unlines $
+            snapurl :
+            [maybe "" show mstart | status /= "STARTED"] ++
+            [maybe "" show mfinish ++ " " ++ status]
+          return $ if "FINISHED" `isPrefixOf` status
+                   then Nothing
+                   else Just False
+          where
+            httpMaybeLastModified url = do
+              exists <- httpExists' url
+              if exists
+                then fmap (utcToZonedTime tz) <$> httpLastModified' url
+                else return Nothing
 
     getComposeFile url file =
       parseRequest (url +/+ file)
