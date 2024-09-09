da5e4856bf20eadeb70e89f5532d4779b0ea6d1a
Jens Petersen <juhpetersen@gmail.com>
2024-09-09 23:27:05 +0800

    tests: bump versions

--- test/tests.hs
+++ test/tests.hs
@@ -17,15 +17,16 @@ tests =
      [["rawhide"]
      ,["-r", "updates"]
      ,["-l2", "updates"]
-     ,["updates", "fedora-39"]
+     ,["updates", "fedora-40"]
      ])
   ,
     ("status",
      [["updates", "-n"]
-     ,["updates", "-n", "fedora-38"]
+     ,["updates", "-n", "fedora-40"]
+     ,["updates", "-n", "fedora-41"]
      ,["rawhide", "-L"]
      ,["branched", "-l2"]
-     ,["branched", "-n", "39"]
+     ,["branched", "-n", "41"]
      ])
   ]
 
